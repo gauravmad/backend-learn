3b0566eec4c9ef0020eca47a775582ef8fea4646
Gaurav Madan <103850488+gauravmad@users.noreply.github.com>
2024-02-26 02:05:52 +0530

    initial

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to Mongo Successfully");
+
+    } catch (error) {
+        console.error("Error connecting to Mongo:",error)
+    }
+};
+
+module.exports = connectToMongo;
